795ef7f4a46826108db0a5ce1d985d4b1fb6f82b
MiaTian1024 <sddxtm123@gmail.com>
2023-07-01 21:54:47 +1200

    setup main and editor

---
+++ editor.py
@@ -0,0 +1,16 @@
+import pygame, sys
+from settings import *
+
+class Editor:
+    def __init__(self):
+        # main setup
+        self.display_surface = pygame.display.get_surface()
+
+    def event_loop(self):
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+    def run(self, dt):
+        self.display_surface.fill('white')

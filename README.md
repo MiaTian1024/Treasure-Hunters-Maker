191ed4da768f49415d6403b6ac8c42ec537b541b
MiaTian1024 <sddxtm123@gmail.com>
2023-07-02 20:29:06 +1200

    create canvas tile, get the dic of canvas data

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
 # Treasure-Hunters-Pygame
 
 ## Diary
-### 1 July, Initial setup and create origin and support lines 
-Add settings.py to store different variables. Create origin and draw support lines, change mouse cursor effect. <br>
+### 1 July - initial setup 
+Add settings.py to store different variables. Create origin point and draw support lines. Change mouse cursor effect. <br>
 
-### 2 July, create menu 
-Seperate menu.py file focus on creating menu buttons. 
-Menu area located in the right buttom of the screen with 4 buttons (tile, coin, enemy and palm). Each button has different buton surfaces which can be toggled and switched. add highlight function if one button has been selected. By pressing right mouse button to switch different button surface. For palm buttons, there are 2 group images (front and back) which can be selected by pressing middle mouse button. Followings are screenshot of menu bar. <br>
+### 2 July - create menu 
+Create a seperate menu.py file. Menu area located in the right buttom of the screen with 4 buttons (tile, coin, enemy and palm). Each button has different buton surfaces which can be toggled and switched. Add highlight function if one button has been selected. By pressing right mouse button to switch different button surfaces. For palm buttons, there are 2 group images (front and back) which can be selected by pressing middle mouse button. Followings are screenshots of menu bar. <br>
 <img src="screenshot/menu1.png" alt="Screenshot" style="width: 400px; height: auto;"> 
 <img src="screenshot/menu2.png" alt="Screenshot" style="width: 400px; height: auto;"> <br>
--- code/editor.py
+++ code/editor.py
@@ -10,6 +10,7 @@ class Editor:
     def __init__(self):
         # main setup
         self.display_surface = pygame.display.get_surface()
+        self.canvas_data ={}
 
         # navigation
         self.origin = vector()
@@ -23,9 +24,25 @@ class Editor:
 
         # selection
         self.selection_index = 2
+        self.last_selected_cell = None
 
         # menu
         self.menu = Menu()
+
+    # support
+    def get_current_cell(self):
+        distance_to_origin = vector(mouse_pos()) - self.origin
+
+        if distance_to_origin.x > 0:
+            col = int(distance_to_origin.x / TILE_SIZE)
+        else:
+            col = int(distance_to_origin.x / TILE_SIZE) - 1
+        if distance_to_origin.y > 0:
+            row = int(distance_to_origin.y / TILE_SIZE)
+        else:
+            row = int(distance_to_origin.y / TILE_SIZE) - 1
+
+        return col, row
     
     # input
     def event_loop(self):
@@ -36,6 +53,7 @@ class Editor:
             self.pan_input(event)
             self.selection_hotkeys(event)
             self.menu_click(event)
+            self.canvas_add()
 
     def pan_input(self, event):
 
@@ -69,7 +87,17 @@ class Editor:
     def menu_click(self, event):
         if event.type == pygame.MOUSEBUTTONDOWN and self.menu.rect.collidepoint(mouse_pos()):
             self.selection_index = self.menu.click(mouse_pos(), mouse_buttons())
-
+ 
+    def canvas_add(self):
+        if mouse_buttons()[0] and not self.menu.rect.collidepoint(mouse_pos()):
+            current_cell = self.get_current_cell()
+            if current_cell != self.last_selected_cell:
+                if current_cell in self.canvas_data:
+                    self.canvas_data[current_cell].add_id(self.selection_index)
+                else:
+                    self.canvas_data[current_cell] = CanvasTile(self.selection_index)
+                self.last_selected_cell = current_cell
+            print(self.canvas_data)
     # drawing
     def draw_tile_lines(self):
         cols = WINDOW_WIDTH // TILE_SIZE
@@ -100,3 +128,34 @@ class Editor:
         self.draw_tile_lines()
         pygame.draw.circle(self.display_surface, 'red', self.origin, 10)
         self.menu.display(self.selection_index)
+
+
+class CanvasTile:
+    def __init__(self, tile_id):
+
+        # terrain
+        self.has_terrain = False
+        self.terrain_neighbors = []
+
+        # water
+        self.has_water = False
+        self.water_on_top = False
+
+        # coin
+        self.coin = None
+
+        # enemy
+        self.enemy = None
+
+        # objects
+        self.objects = []
+
+        self.add_id(tile_id)
+
+    def add_id(self, tile_id):
+        options = {key: value['style'] for key, value in EDITOR_DATA.items()}
+        match options[tile_id]:
+            case 'terrain': self.has_terrain = True
+            case 'water': self.has_water = True
+            case 'coin': self.coin = tile_id
+            case 'enemy': self.enemy = tile_id

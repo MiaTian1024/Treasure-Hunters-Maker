60fc67fbdc9d30bde405dd2f97c1ce82f3fd21d0
MiaTian1024 <sddxtm123@gmail.com>
2023-07-04 10:35:06 +1200

    delete tiles by clicking right mouse

--- code/editor.py
+++ code/editor.py
@@ -50,7 +50,6 @@ class Editor:
             row = int(distance_to_origin.y / TILE_SIZE) - 1
 
         return col, row
-     
     
     def check_neighbors(self, cell_pos):
 
@@ -79,7 +78,6 @@ class Editor:
                         if self.canvas_data[neighbor_cell].has_terrain:
                             self.canvas_data[cell].terrain_neighbors.append(name)
 
-
     def imports(self):
         self.water_bottom = load('../graphics/terrain/water/water_bottom.png')
 
@@ -100,7 +98,6 @@ class Editor:
             if value['frame index'] >= value['length']:
                 value['frame index'] = 0
 
-
     # input
     def event_loop(self):
         for event in pygame.event.get():
@@ -111,6 +108,7 @@ class Editor:
             self.selection_hotkeys(event)
             self.menu_click(event)
             self.canvas_add()
+            self.canvas_remove()
 
     def pan_input(self, event):
 
@@ -157,6 +155,18 @@ class Editor:
                     self.canvas_data[current_cell] = CanvasTile(self.selection_index)
                 self.check_neighbors(current_cell)
                 self.last_selected_cell = current_cell
+
+    def canvas_remove(self):
+        if mouse_buttons()[2] and not self.menu.rect.collidepoint(mouse_pos()):
+            if self.canvas_data:
+                current_cell = self.get_current_cell()
+                if current_cell in self.canvas_data:
+                    self.canvas_data[current_cell].remove_id(self.selection_index)
+
+                    if self.canvas_data[current_cell].is_empty:
+                        del self.canvas_data[current_cell]
+                    self.check_neighbors(current_cell)
+                  
            
     # drawing
     def draw_tile_lines(self):
@@ -251,6 +261,7 @@ class CanvasTile:
         self.objects = []
 
         self.add_id(tile_id)
+        self.is_empty = False
 
     def add_id(self, tile_id):
         options = {key: value['style'] for key, value in EDITOR_DATA.items()}
@@ -259,3 +270,16 @@ class CanvasTile:
             case 'water': self.has_water = True
             case 'coin': self.coin = tile_id
             case 'enemy': self.enemy = tile_id
+
+    def remove_id(self, tile_id):
+        options = {key: value['style'] for key, value in EDITOR_DATA.items()}
+        match options[tile_id]:
+            case 'terrain': self.has_terrain = False
+            case 'water': self.has_water = False
+            case 'coin': self.coin = None
+            case 'enemy': self.enemy = None
+        self.check_content()
+
+    def check_content(self):
+        if not self.has_terrain and not self.has_water and not self.coin and not self.enemy:
+            self.is_empty = True
